cc76093a81ce6ae34588701ab334b4e25617e03f
shaowenhu <swhu0330@163.com>
2020-05-24 11:51:53 +0800

    添加项目文件。

---
+++ InputOutput/InputOutput.cpp
@@ -0,0 +1,19 @@
+﻿//标准输入输出
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //char name[50];
+    string name;
+    cout << "Input name:";
+    cin >> name;
+    cout << "Your Input name:" << name << endl;
+
+    int A;
+    cout << "Input number A:";
+    cin >> A;
+    cout << "Your Inpur number A:" << A;
+
+}
